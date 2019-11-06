9513b976caab5e8392db56ff3685ebd4cc711eeb
irving <huan.guo@silot.ai>
2019-11-06 17:45:27 +0800

    code optimization

--- xxl-job-executor/src/main/java/com/xxl/job/executor/jobhandler/HttpJobHandler.java
+++ xxl-job-executor/src/main/java/com/xxl/job/executor/jobhandler/HttpJobHandler.java
@@ -1,20 +1,12 @@
 package com.xxl.job.executor.jobhandler;
 
-import com.google.common.collect.Collections2;
 import com.google.common.collect.Maps;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
-import com.xxl.job.core.log.XxlJobLogger;
 import com.xxl.job.executor.common.utils.HttpUtil;
 import com.xxl.job.executor.common.utils.http.Response;
 import org.springframework.stereotype.Component;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Collections;
 
 /**
  * 跨平台Http任务
