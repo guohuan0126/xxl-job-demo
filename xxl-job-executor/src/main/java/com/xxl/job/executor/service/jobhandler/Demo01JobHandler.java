0d3c9bde37d14f669f6e5fc9c007f308ec6f738b
irving <huan.guo@silot.ai>
2019-11-06 11:59:14 +0800

    first commit

---
+++ xxl-job-executor/src/main/java/com/xxl/job/executor/service/jobhandler/Demo01JobHandler.java
@@ -0,0 +1,30 @@
+package com.xxl.job.executor.service.jobhandler;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.JobHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @program: xxl-job
+ * @description:
+ * @author: Irving
+ * @create: 2019-11-05
+ **/
+@JobHandler(value = "demo01JobHandler")
+@Component
+public class Demo01JobHandler extends IJobHandler {
+    @Override
+    public ReturnT<String> execute(String param){
+
+        System.out.println("demo01执行，执行时间：" + new Date());
+        System.out.println("param:"+param);
+        if ("5".equals(param)) {
+            throw new NullPointerException();
+        }
+
+        return SUCCESS;
+    }
+}
